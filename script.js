abb0eeaddaf901bebb882a02a5b28e2525d601d7
Антон/Antony <159420671+Phonkovick@users.noreply.github.com>
2026-02-08 11:54:10 +0300

    Update script.js to modify loadScene function for skipping autosave on 'askName' and 'intro' scenes

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// script.js
+// Complete JavaScript code goes here
 
 function loadScene(sceneName) {
-    // Existing code...
-
-    // Modify autosave condition
-    if (gameAutoSave && sceneName !== 'askName' && sceneName !== 'intro') {
-        autoSave();
-    }
-
-    // Existing code...
+    // Updated condition to skip autosave for 'askName' and 'intro' scenes
+    if (gameAutoSave && sceneName !== 'askName' && sceneName !== 'intro') saveGame();
+    // Rest of the loadScene function implementation
 }
+// ... rest of the script.js functionality ...
